bc7472c75f019926c5262d528d9c0ae5bf1cdad5
Vitalijus <96495591+Rozietka@users.noreply.github.com>
2022-03-31 21:04:00 +0300

    Get service data validation

--- components/servicesSection.js
+++ components/servicesSection.js
@@ -1,4 +1,5 @@
 import { file } from '../lib/file.js';
+import { utils } from '../lib/utils.js';
 
 async function servicesSection() {
     const getServicesData = async () => {
@@ -7,20 +8,30 @@ async function servicesSection() {
         if (err) {
             return data;
         }
-        console.log(servicesFiles);
 
-        // perskaityti kiekvieno to failo turini
-        // turini sudeti i masyva
+        for (const serviceFileName of servicesFiles) {
+            const [err, content] = await file.read('services', serviceFileName);
+            if (err) {
+                continue;
+            }
+
+            let obj = utils.parseJSONtoObject(content);
+            if (!obj) {
+                continue;
+            }
+
+            data.push(obj);
+        }
+
         return data;
     }
 
-    const renderList = () => {
-        const servicesData = getServicesData();
+    const renderList = async () => {
+        const servicesData = await getServicesData();
         if (!Array.isArray(servicesData) ||
             servicesData.length === 0) {
             return '';
         }
-
         let HTML = '';
         for (const service of servicesData) {
             HTML += `<div class="service">
@@ -36,7 +47,8 @@ async function servicesSection() {
                     <h2>Services</h2>
                     <p>Each time a digital asset is purchased or sold, Sequoir donates a percentage of the fees back into the development of the asset through its charitable foundation.</p>
                 </div>
-                <div class="row services-list">${renderList()}</div>
+                <div class="row services-list">${await renderList()}</div>
             </section>`;
 }
+
 export { servicesSection };
