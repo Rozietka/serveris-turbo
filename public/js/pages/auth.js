7b17701c84e0b14436916aeec2d7a3f6d1881285
Vitalijus <96495591+Rozietka@users.noreply.github.com>
2022-04-05 21:07:42 +0300

    Form validation

--- public/js/pages/auth.js
+++ public/js/pages/auth.js
@@ -1,39 +1,38 @@
+import { IsValid } from '../components/is-valid/IsValid.js';
 /*
-1) susirasti forma ir jos VISUS LAUKUS
-2) susirinkti informacija is formos
+1) susirasti forma ir jos VISUS laukus
+2) surinkti informacija is formos
 3) FE validacija
 4a) jei yra klaidu - atvaizduojame
-4b) jie nera klaidu - siunciame info objekta i serveri
-5) is serverio gauta atsakyma interpretuojame ir kazka darome
+4b) jei nera klaidu - siunciame info objekta i serveri
+5) is serverio gauta atsakima interpretuojame ir kazka darome
 6a) jei serveris rado klaidu - atvaizduojame
-6b) jei serveris nerado klaidu - END
- */
-
-const formDOM = document.querySelector('form');
-const errorsDOM = document.querySelector('form-errors');
+6b) jei serveris ne rado klaidu - END
+*/
+const formDOM = document.querySelector('.form');
+const errorsDOM = formDOM.querySelector('.form-errors');
 const allInputsDOM = formDOM.querySelectorAll('input');
 const submitDOM = formDOM.querySelector('button');
-
 submitDOM.addEventListener('click', (e) => {
     e.preventDefault();
 
-   const formData = {};
-
+    const errors = [];
+    const formData = {};
     for (const inputDOM of allInputsDOM) {
-        const { id, value } = inputDOM;
+        const { id, value, dataset } = inputDOM;
+        const validationRule = IsValid[dataset.validation];
+        const [err, status] = validationRule(value);
+        if (err) {
+            errors.push(status);
+        }
         formData[id] = value;
-       
     }
-    if (formData.pass === formData.repass) {
-        errorsDOM.innerText = '';
-    } else {
-        errorsDOM.innerText = 'Nesutampa slaptazodziai';
-        return;
+
+    if (formData.pass !== formData.repass) {
+        errors.push('Nesutampa slaptazodziai');
     }
+
+    errorsDOM.innerText = errors.join('\r\n');
 
     console.log('SIUNCIAME I SERVERI:', formData);
 })
-
-
-
-
