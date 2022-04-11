cc715ae813898073dbe6ac9987a80d1f47c01266
Vitalijus <96495591+Rozietka@users.noreply.github.com>
2022-04-11 19:24:52 +0300

    Token API: post method full validation

--- api/token.js
+++ api/token.js
@@ -1,5 +1,9 @@
+import { file } from "../lib/file.js";
 import { IsValid } from "../lib/IsValid.js";
+import { utils } from "../lib/utils.js";
+
 const handler = {};
+
 handler.token = (data, callback) => {
     const acceptableMethods = ['get', 'post', 'put', 'delete'];
     if (acceptableMethods.includes(data.httpMethod)) {
@@ -7,12 +11,13 @@ handler.token = (data, callback) => {
     }
     return callback(400, 'Token: veiksmas NEleistinas');
 }
+
 handler._method = {};
-handler._method.post = (data, callback) => {
+
+handler._method.post = async (data, callback) => {
     const user = data.payload;
     const { email, password } = user;
     const requiredKeys = 2;
-
     const [emailErr, emailMsg] = IsValid.email(email);
     if (emailErr) {
         return callback(400, {
@@ -27,7 +32,6 @@ handler._method.post = (data, callback) => {
             msg: passwordMsg,
         })
     }
-
     if (requiredKeys !== Object.keys(user).length) {
         return callback(400, {
             status: 'Error',
@@ -35,11 +39,46 @@ handler._method.post = (data, callback) => {
         })
     }
 
+    const [readErr, readMsg] = await file.read('accounts', email + '.json');
+    if (readErr) {
+        return callback(400, {
+            status: 'Error',
+            msg: 'Klaida bandant prisijungti: nesutampa email arba slaptazodis',
+        })
+    }
+
+    const userObj = utils.parseJSONtoObject(readMsg);
+    if (!userObj) {
+        return callback(500, {
+            status: 'Error',
+            msg: 'Klaida bandant nuskaityti vartotojo duomenis',
+        })
+    }
+
+    const hashedPassword = utils.hash(password);
+    if (hashedPassword !== userObj.password) {
+        return callback(400, {
+            status: 'Error',
+            msg: 'Klaida bandant prisijungti: nesutampa email arba slaptazodis',
+        })
+    }
+
+    const token = {};
+
+    const [createErr] = await file.create('token', 'token.json', token);
+    if (createErr) {
+        return callback(500, {
+            status: 'Error',
+            msg: 'Klaida bandant isduoti sesijos Token',
+        })
+    }
+
     return callback(200, {
-        action: 'POST',
-        msg: 'Vartotojui isduotas sesijos TOKEN',
+        status: 'Success',
+        msg: token,
     })
 }
+
 handler._method.get = (data, callback) => {
     return callback(200, {
         action: 'GET',
