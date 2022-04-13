8d5fbc3e1c84d3385fa3f52be2aa5b9ec97065a9
Vitalijus <96495591+Rozietka@users.noreply.github.com>
2022-04-13 20:10:36 +0300

    Token JSON cleanup

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import { server } from './lib/server.js';
+import config from './config.js';
+
 const app = {};
 app.init = () => {
     // pasiruosti pradinius folder'ius
@@ -10,7 +12,7 @@ app.init = () => {
     // reguliariu procesu paleidimas:
     // - istrinti senus/nebereikalingus failus
     server.cleanUp();
-    setInterval(server.cleanUp, 60 * 1000);
+    setInterval(server.cleanUp, config.cookiesMaxAge * 1000);
     // - maziau naudojamu failu archivavimas
     // - atsinaujinti informacija per/is API
 }
