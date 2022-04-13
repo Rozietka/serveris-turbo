82752243e93cebe1cbec434ede71e583c29fd61f
Vitalijus <96495591+Rozietka@users.noreply.github.com>
2022-04-13 20:08:49 +0300

    Server clean up initial function logic

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 import { server } from './lib/server.js';
-
 const app = {};
-
 app.init = () => {
     // pasiruosti pradinius folder'ius
-
     // pasiruosti pradinius failus
-
     // prisijungimas prie DB (duomenu baze)
-
     // uzkurti pati serveri (musu programa)
     server.init();
 
     // reguliariu procesu paleidimas:
     // - istrinti senus/nebereikalingus failus
+    server.cleanUp();
+    setInterval(server.cleanUp, 60 * 1000);
     // - maziau naudojamu failu archivavimas
     // - atsinaujinti informacija per/is API
 }
-
 app.init();
-
 export { app };
