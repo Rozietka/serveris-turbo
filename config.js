821c7b1e1e27e71dc29e28b24728c51e7470c01b
Vitalijus <96495591+Rozietka@users.noreply.github.com>
2022-04-20 21:16:05 +0300

    Extending Token cookies in BE

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ environments.dev = {
     },
     password: 'demo-password',
     hashingSecret: '847jh5ge2r54yj152ty8y4',
-    cookiesMaxAge: 3600,
+    cookiesMaxAge: 10,
     sessionTokenLength: 10,
 }
 
