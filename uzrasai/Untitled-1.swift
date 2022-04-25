a86e432668887c44d2dd9621f876d226adbf455d
Vitalijus <96495591+Rozietka@users.noreply.github.com>
2022-04-25 21:40:31 +0300

    Update

---
+++ uzrasai/Untitled-1.swift
@@ -0,0 +1,33 @@
+/ -> home
+
+/about -> about
+
+/contact -> contact
+
+/blog -> blog list
+
+/blog/:blogTitle -> single blog post
+
+/blog/:blogTitle/komentarai -> single blog post komentarai
+
+/blog/:blogTitle/autorius -> single blog post autorius
+
+/team -> team list
+
+/team/:member -> single team member
+
+
+
+/blog/a -> single blog post   -> {blogTitle: a}
+
+/blog/b -> single blog post   -> {blogTitle: b}
+
+/blog/c -> single blog post   -> {blogTitle: c}
+
+
+
+/team/a -> single team member   -> {member: a}
+
+/team/b -> single team member   -> {member: b}
+
+/team/c -> single team member   -> {member: c}
