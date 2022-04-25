a158fe21915b2c74d4a9de0c3784c2998830282b
Vitalijus <96495591+Rozietka@users.noreply.github.com>
2022-04-25 20:37:05 +0300

    Initial blog post page

--- pages/PageBlog.js
+++ pages/PageBlog.js
@@ -1,7 +1,6 @@
 import { file } from "../lib/file.js";
 import { PageTemplate } from "../lib/PageTemplate.js";
 import { utils } from "../lib/utils.js";
-
 class PageBlog extends PageTemplate {
     /**
      * Sabloninio puslapio konstruktorius.
@@ -12,25 +11,20 @@ class PageBlog extends PageTemplate {
         super(data);
         this.pageCSSfileName = 'blog';
     }
-
     async getBlogData() {
         const [listErr, list] = await file.list('blog');
         if (listErr) {
             return [];
         }
-
         const blogData = [];
-
         for (const fileName of list) {
             if (fileName[0] === '.') {
                 continue;
             }
-
             const [readErr, readContent] = await file.read('blog', fileName);
             if (readErr) {
                 continue;
             }
-
             const obj = utils.parseJSONtoObject(readContent);
             if (!obj) {
                 continue;
@@ -38,16 +32,14 @@ class PageBlog extends PageTemplate {
             blogData.push(obj);
         }
 
-        return blogData;
+        return blogData.sort((a, b) => b.lastUpdated - a.lastUpdated);
     }
 
     emptyListHTML() {
         return `<div class="row empty-list">Looks like blog list is empty right now 🤔👀😭</div>`;
     }
-
     blogListHTML(data) {
         let HTML = '';
-
         for (const post of data) {
             HTML += `<div class="post">
                         <h2 class="post-title">${post.title}</h2>
@@ -55,25 +47,20 @@ class PageBlog extends PageTemplate {
                         <a href="/blog/${post.slug}" class="read-more">Read more<i class="icon fa fa-angle-right"></i></a>
                     </div>`;
         }
-
         return `<div class="row list">${HTML}</div>`;
     }
-
     shortenText(text) {
         const limit = 100;
         const hardLimit = 130;
         if (text.length < hardLimit) {
             return text;
         }
-
         let partText = text.slice(0, limit);
         partText = partText.split('').reverse().join('');
         partText = partText.slice(partText.indexOf(' ') + 1);
         partText = partText.split('').reverse().join('');
-
         return partText + '...';
     }
-
     async mainHTML() {
         const blogFiles = await this.getBlogData();
         return `<section class="container blog-list">
--- pages/PageBlogPost.js
+++ pages/PageBlogPost.js
@@ -1,5 +1,4 @@
 import { PageTemplate } from "../lib/PageTemplate.js";
-import { file } from '../lib/file.js';
 
 class PageBlogPost extends PageTemplate {
     /**
@@ -12,24 +11,12 @@ class PageBlogPost extends PageTemplate {
         this.pageCSSfileName = 'blog-post';
     }
 
-    async getBlogPostData() {
-        const postSlug = this.data.trimmedPath.split('/')[1].trim();
-        try {
-            const fileContent = await file.read('/data/blog-posts', postSlug + '.json');
-            const contentObj = utils.parseJSONtoObject(fileContent);
-            return contentObj;
-        } catch (error) {
-            return false;
-        }
-    }
-
     badPostHTML() {
         return `<section class="container blog-inner">
                     <h1 class="row title">500</h1>
                     <p class="row">Something's wrong with server. Please, come back later.</p>
                 </section>`;
     }
-
     correctPostHTML(post) {
         return `<section class="container blog-inner">
                     <h1 class="row title">${post.title}</h1>
@@ -37,7 +24,6 @@ class PageBlogPost extends PageTemplate {
                     <footer class="row">Author</footer>
                 </section>`;
     }
-
     isValidPost(post) {
         if (typeof post !== 'object'
             || Array.isArray(post)
@@ -47,13 +33,17 @@ class PageBlogPost extends PageTemplate {
         return true;
     }
 
-    async mainHTML() {
-        const postData = await this.getBlogPostData();
-        if (this.isValidPost(postData)) {
-            return this.correctPostHTML(postData);
+    mainHTML() {
+        if (false) {
+            return this.correctPostHTML();
         } else {
             return this.badPostHTML();
         }
+        // if (this.isValidPost(postData)) {
+        //     return this.correctPostHTML(postData);
+        // } else {
+        //     return this.badPostHTML();
+        // }
     }
 }
 
